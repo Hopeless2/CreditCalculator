9db3e5e6790a82e5f967c0a50f29e1148c8ae91f
Hopeless2 <89778966354@mail.ru>
2021-12-25 15:49:47 +0300

    Tests added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    private static double CREDIT_PERCENT_PER_YEAR = 10.00;
+
+    public Calculator() {
+
+    }
+
+    public double calculateMonthlyPayment(double total, int months) {
+        return 0.00;
+    }
+
+    public double calculateTotalPayment(double total, int months) {
+        return 0.00;
+    }
+
+    public double calculateOverpayment(double total, int months) {
+        return 0.00;
+    }
+
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.*;
+
+public class CalculatorTest {
+    private static long suiteStartTime;
+    private long testStartTime;
+    private static Calculator calc = new Calculator();
+
+    @BeforeAll
+    public static void initSuite() {
+        System.out.println("Running CalculatorTest");
+        suiteStartTime = System.nanoTime();
+    }
+
+    @AfterAll
+    public static void completeSuite() {
+        System.out.println("CalculatorTest complete: " + (System.nanoTime() - suiteStartTime));
+    }
+
+    @BeforeEach
+    public void initTest() {
+        System.out.println("Starting new test");
+        testStartTime = System.nanoTime();
+    }
+
+    @AfterEach
+    public void finalizeTest() {
+        System.out.println("Test complete:" + (System.nanoTime() - testStartTime));
+    }
+
+    @Test
+    public void calculateMonthlyPaymentTest_success() {
+        //given:
+        double expected = 4_583.33;
+        double total = 50_000.00;
+        int months = 12;
+        //when:
+        double result = calc.calculateMonthlyPayment(total, months);
+        //then:
+        Assertions.assertTrue(Math.abs(expected - result) < 0.0000001);
+    }
+
+    @Test
+    public void calculateTotalPaymentTest_success() {
+        //given:
+        double expected = 55_000.00;
+        double total = 50_000.00;
+        int months = 12;
+        //when:
+        double result = calc.calculateTotalPayment(total, months);
+        //then:
+        Assertions.assertTrue(Math.abs(expected - result) < 0.0000001);
+    }
+
+    @Test
+    public void calculateOverpaymentTest_success() {
+        //given:
+        double expected = 5_000.00;
+        double total = 50_000.00;
+        int months = 12;
+        //when:
+        double result = calc.calculateOverpayment(total, months);
+        //then:
+        Assertions.assertTrue(Math.abs(expected - result) < 0.0000001);
+    }
+
+
+}
